3c5149cee0683cb1dfe64ad1515f7599a5726c05
Rifa9oct <kohinuraktherr@gmail.com>
2024-01-27 00:31:50 +0600

    added banner and footer section

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,13 @@
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
+            <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
