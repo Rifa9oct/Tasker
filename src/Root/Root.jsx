28c39147cdd6ad7b518727af65a8823f8745031b
Rifa9oct <kohinuraktherr@gmail.com>
2024-01-27 01:06:01 +0600

    added TaskModal and TaskBoard section

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,14 @@
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import TaskBoard from "../components/TaskBoard/TaskBoard";
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
             <Banner></Banner>
+            <TaskBoard></TaskBoard>
             <Footer></Footer>
         </div>
     );
